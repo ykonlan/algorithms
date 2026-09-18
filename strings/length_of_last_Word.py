f81d41ea5248be5e2061403bfa6ae847e8b7600d
Yoeman <yoemankonlan@gmail.com>
2026-09-18 23:51:56 +0000

    length of last word

---
+++ strings/length_of_last_Word.py
@@ -0,0 +1,14 @@
+""" Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only. """
+
+def lengthOfLastWord(s: str) -> int:
+        i = len(s) - 1
+        length = 0
+        while i >= 0 and s[i] == " ":
+            i -= 1
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
+        return length 
+
